1ab227fa4f558bd1926349ee56c4a6f03f614eac
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-06-05 11:18:48 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -20,7 +20,7 @@ class Application
         resp.write "Your cart is empty"
       else
         @@cart.each do |item|
-          "#{item}\n"
+          resp.write "#{item}\n"
         end
       end
       elsif req.path.match(/add/)
@@ -46,3 +46,45 @@ class Application
     end
   end
 end
+
+# # Create a new route called /add that takes in a GET param with the key item. This should check to see if that item is in @@items and then add it to the cart if it is. Otherwise give an error
+# if req.path.match(/items/)
+#     @@items.each do |item|
+#       resp.write "#{item}\n"
+#     end
+#   elsif req.path.match(/search/)
+#     search_term = req.params["q"]
+#     resp.write handle_search(search_term)
+#   elsif req.path.match(/cart/)
+#     if @@cart.empty?
+#       resp.write "Your cart is empty"
+#     else
+#       @@cart.each do |item|
+#         resp.write "#{item}\n"
+#       end
+#     end
+#   elsif req.path.match(/add/)
+#     item_to_add = req.params["item"]
+#     if @@items.include? item_to_add
+#       @@cart << item_to_add
+#       resp.write "added #{item_to_add}"
+#     else
+#       resp.write "We don't have that item!"
+#     end
+#   else
+#     resp.write "Path Not Found"
+#   end
+#
+#   resp.finish
+# end
+#
+# def handle_search(search_term)
+#   if @@items.include?(search_term)
+#     return "#{search_term} is one of our items"
+#   else
+#     return "Couldn't find #{search_term}"
+#   end
+# end
+# end
+# Contact GitHub API Training Shop Blog About
+# © 2017 GitHub, Inc. Terms Privacy Security Status Help
